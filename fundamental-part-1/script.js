0bdf3f7da1f948f3474bd08f757e311347173b7a
willson733 <willsonghimire58@gmail.com>
2023-01-04 17:54:32 +0545

    fundamental part 1

---
+++ fundamental-part-1/script.js
@@ -0,0 +1,56 @@
+/*
+//linking a javascript file
+let js="amazing";
+if (js==="amazing") alert("js is fun");
+console.log(js);
+//values and variables
+console.log('willson');
+console.log(123);
+
+
+let firstNmae="willson";
+console.log(firstNmae);
+console.log(firstNmae);
+console.log(firstNmae);
+
+let yourName=prompt("what is your name")
+alert(yourName);
+console.log(yourName);
+// assigment
+let country="Nepal";
+let continent="Asia";
+let population="2million";
+console.log(alert("country= "+country));
+console.log("continent= "+continent);
+console.log("population= "+population);
+*/
+
+// variables
+let javaScriptIsFun=true;
+console.log(javaScriptIsFun);
+console.log(typeof javaScriptIsFun);
+
+console.log(typeof "willson");
+console.log(typeof 123);
+ 
+
+javaScriptIsFun="willson"; //dynamic data type
+console.log(javaScriptIsFun);
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null)
+
+
+//assigment
+let isIsland=false;
+let language;
+let population=2000000;
+let country="Nepal"
+console.log(typeof isIsland );
+console.log(typeof language );
+console.log(typeof population );
+console.log(typeof country);
