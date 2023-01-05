8dfde2a62aaf1c1c2abf6040d986bdeecb8da327
willson733 <willsonghimire58@gmail.com>
2023-01-05 22:08:47 +0545

    fundamental part one  added

--- fundamental-part-1/script.js
+++ fundamental-part-1/script.js
@@ -24,7 +24,7 @@ console.log(alert("country= "+country));
 console.log("continent= "+continent);
 console.log("population= "+population);
 */
-
+/*
 // variables
 let javaScriptIsFun=true;
 console.log(javaScriptIsFun);
@@ -54,3 +54,176 @@ console.log(typeof isIsland );
 console.log(typeof language );
 console.log(typeof population );
 console.log(typeof country);
+*/
+/*
+//let const var 
+let age=23;
+age=25;
+console.log(age);
+
+const birthYear=2000;
+// birthYear=2001 //constant variable cannot be changed
+
+var yourName="willson";
+yourName="loveson"
+console.log(yourName);
+
+*/
+
+
+/*
+// opetrators
+
+// maths operator
+const now=2023
+const willsonAge=now-2000;
+const lovesonAge=now-2006;
+console.log(willsonAge,lovesonAge);
+
+console.log(willsonAge*2 , willsonAge/5, 2**3)
+// 2**3 means two to the power 3
+const firstNmae='willson';
+const lastName='ghimire';
+console.log(firstNmae+lastName);
+console.log(firstNmae+ " " +lastName);
+
+// assigment operator
+let x=15+10;//x=25
+x+=5;//x=x+5
+x++;//x=x+1
+x--;
+x--;
+
+console.log(x);
+
+// comparision operator
+console.log(willsonAge>lovesonAge);
+console.log(lovesonAge>=16);
+
+// assignment
+let population=10000000;
+console.log(population/2);
+population+=1;
+console.log(population);
+console.log(population>6000000);
+console.log(population<33000000);
+let description="Portugal is in Europe " ;
+console.log(description);
+*/
+
+/*
+// string and templete literal
+
+const firstName='willson';
+const job='teacher';
+const birthYear=2000;
+const year='2023';
+
+console.log("I'm " + firstName + ", a " + job +" " +(year-birthYear) + " old");
+
+const newNmae=`I'm ${firstName}, a ${job} ${year-birthYear} old`;
+console.log(newNmae);
+
+
+console.log("string\n\with\n\multiple lines ");
+console.log(`string
+with
+multiple lines`);*/
+
+/*
+// if else statement
+const age=19;
+if(age>=18){
+    console.log(`you can able to drive🚗`);
+}
+else{
+    const yearLeft=18-age;
+    console.log(`you should wait ${yearLeft} years to drive`)
+}
+
+const birthYear=1990;
+let century;
+if(birthYear<=2000){
+    century=20;
+}
+else{
+    century=21;
+}
+console.log(`this is ${century} century`);
+*/
+
+/*
+// type conversion
+const inputYear='1990';
+console.log(Number(inputYear), inputYear);
+console.log(inputYear +  20);
+console.log(Number(inputYear) + 20);
+
+// type coercion
+console.log('I am '+ 25 +' years old')
+console.log('10'+5);
+console.log(10+3+2+'10')
+console.log('10'-5); 
+*/
+
+
+/*
+// 5 falsy value: 0, '', undefined, null, NaN 
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('willson'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money=0;
+if(money){
+    console.log(`Don't spend it all!`);
+
+}
+else{
+    console.log(`You should get a job`);
+}
+let height=1;
+if(height){
+    console.log(`YEH Height is difined`);
+
+}
+else{
+    console.log(`Height is undefined`);
+}
+*/
+
+/*
+
+// equility operator
+const age=23;
+if(age===23) console.log(`your age is 23:(strictly)`);
+const ages="23";
+if(ages==23) console.log(`your age is 23 (loosly)`); 
+
+const favNumber=prompt("what is your favorite number?");
+console.log(favNumber);
+if(favNumber==="23"){
+    console.log(`your fav number is 23 `);
+}
+
+const favNum=Number(prompt("what is your favorite number?"));
+if(favNum===23){
+    console.log(`your fav number is 23 `);
+}
+else if(favNum===7){
+    console.log('your fav number is 7');
+} 
+else if(favNum===9){
+    console.log('your fav number is 9');
+}
+else {
+    console.log('your fav number is not 9 or 7 or 27');
+} 
+if(favNum!==23){
+    console.log(`your fav number is not 23`);
+}
+
+*/
+
+// logical operation
