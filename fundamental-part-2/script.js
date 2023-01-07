b83f0905f0a2320ff9e276fce7c5446df23968d2
willson733 <willsonghimire58@gmail.com>
2023-01-07 18:28:55 +0545

    fundamental part 2 added

---
+++ fundamental-part-2/script.js
@@ -0,0 +1,63 @@
+
+/*
+// strict mode activiting
+'use strict';
+let hasDrivingLicenses= false;
+const passTest= true;
+if(passTest) hasDrivingLicense=true;
+if( hasDrivingLicenses) console.log(`You can drive`);
+
+// const interface='audio';
+// const private=40;
+// const if=30;
+*/
+
+/*
+// functions
+
+function logger(){
+    console.log(`My name is willson`);
+}
+logger();//calling,invoking a function
+logger();
+logger();
+
+function fruitProcessor(apple, orange){
+    
+  const  juice=`juice with ${apple} apple and ${orange} orange`;
+  return juice;
+}
+
+const appleJuice=fruitProcessor(4,0);
+console.log(appleJuice);
+
+
+console.log(`${fruitProcessor(5,6)}`);
+
+
+const orangeAppleJuice=fruitProcessor(4, 5);
+console.log(orangeAppleJuice);
+
+*/
+
+/*
+// function decleration and expression
+
+//  decleration
+
+function calcAge1(birthYear){
+    
+    return 2023-birthYear;
+}
+const age1=calcAge1(2000);
+
+
+// expression 
+
+const calcage2=function(birthYear){
+return 2023-birthYear;
+}
+const age2=calcAge1(2000)
+
+console.log(age1, age2);
+*/
