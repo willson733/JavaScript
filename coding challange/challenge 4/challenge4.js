76159b3bbc68e0f98a312917ee84343164fe9033
willson733 <willsonghimire58@gmail.com>
2023-01-07 22:18:36 +0545

    removed

--- coding challange/challenge 4/challenge4.js
+++
@@ -1,3 +0,0 @@
-const billValue=275;
-const tip=billValue>=50 && billValue<=300?0.15*billValue:0.20*billValue;
-console.log(`The bill was ${billValue}, the tip was ${tip} and the total value ${billValue+tip}`);
