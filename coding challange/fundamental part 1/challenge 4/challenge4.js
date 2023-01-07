77d6d88d5851cbce5341cd43694b91275c0b2126
willson733 <willsonghimire58@gmail.com>
2023-01-07 22:20:18 +0545

    commit

--- coding challange/fundamental part 1/challenge 4/challenge4.js
+++ coding challange/fundamental part 1/challenge 4/challenge4.js
@@ -1,3 +1,3 @@
-const billValue=275;
+const billValue=270;
 const tip=billValue>=50 && billValue<=300?0.15*billValue:0.20*billValue;
 console.log(`The bill was ${billValue}, the tip was ${tip} and the total value ${billValue+tip}`);
