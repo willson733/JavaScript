655b429641d493e2da7c67d336c8757c1739e62e
willson733 <willsonghimire58@gmail.com>
2023-01-18 12:08:16 +0545

    call back function added

--- closer look at function/script.js
+++ closer look at function/script.js
@@ -52,3 +52,28 @@ checkIN(flight, willson);
 console.log(flight);
 console.log(willson);
 */
+
+// Function accepting callback function
+
+const oneword = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+const upperFirstWord = function (str) {
+  const [first, ...other] = str.split(' ');
+  return [first.toUpperCase(), ...other].join(' ');
+};
+// Higher order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string:${fn(str)}`);
+  console.log(`Transformed by:${fn.name}`);
+};
+
+transformer('JavaScript is Best!.', upperFirstWord);
+transformer('JavaScript is the best!.', oneword);
+
+const high5 = function () {
+  console.log('👋');
+};
+document.body.addEventListener('click', high5);
+['willson', 'ghimire', 'dharam'].forEach(high5);
