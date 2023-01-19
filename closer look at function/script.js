482a8bf5b931a5559bfecd5e012908e0fe78c649
willson733 <willsonghimire58@gmail.com>
2023-01-19 11:01:14 +0545

    coding challange added

--- closer look at function/script.js
+++ closer look at function/script.js
@@ -96,6 +96,7 @@ const greetArr = greeting => name => console.log(`${greeting} ${name}`);
 greetArr('hey')('willson');
 */
 
+/*
 // -------------The call, apply and bind methods-----
 
 const lufthansa = {
@@ -159,3 +160,38 @@ lufthansa.buyplane = function () {
 document
   .querySelector('.buy')
   .addEventListener('click', lufthansa.buyplane.bind(lufthansa));
+*/
+
+// ----------coding challange------------
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0]. More in the next section 😃
+  answers: new Array(4).fill(0),
+  // Get answer
+  registerNewanswer() {
+    const answer = Number(
+      prompt(
+        `${this.question}\n ${this.options.join(
+          '\n'
+        )} \n What is the option number `
+      )
+    );
+    console.log(answer);
+    //   Register answer
+    typeof answer === 'number' &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+    this.displayResults();
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    if (type === 'array') console.log(this.answers);
+    else if (type === 'string') {
+      console.log(`Pool results are:${this.answers.join(', ')}`);
+    }
+  },
+};
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewanswer.bind(poll));
