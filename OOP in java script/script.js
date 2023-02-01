2dc99e6156235cad8ff82432dc57945a4e0a6d10
willson733 <willsonghimire58@gmail.com>
2023-02-01 17:51:34 +0545

    coding challlange added

--- OOP in java script/script.js
+++ OOP in java script/script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+/*
 // constructor function and the new operator
 
 // Instance property
@@ -21,3 +22,48 @@ const jack = new Person("Jack", 1988);
 console.log(matilda, jack);
 
 console.log(willson instanceof Person);
+
+// Prototypes
+Person.prototype.calcAge = function () {
+  console.log(2023 - this.birthYear);
+};
+willson.calcAge();
+matilda.calcAge();
+
+console.log(willson.__proto__);
+
+console.log(Person.prototype.isPrototypeOf(willson));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+// PrototypeOfLinkedObject
+
+Person.prototype.species = "Homo sapiens";
+console.log(willson.species, matilda.species);
+
+console.log(willson.hasOwnProperty("firstName"));
+console.log(willson.hasOwnProperty("species"));
+*/
+
+// ---------coding challange 1 --------
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+const bmw = new Car("BMW", 120);
+const mercedes = new Car("Mercedes", 95);
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed} km/hr`);
+};
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed} km/hr`);
+};
+
+bmw.accelerate();
+bmw.accelerate();
+bmw.brake();
+bmw.brake();
