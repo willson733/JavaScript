5fa725530713d60358c81b6b2ba8cddd28b3d07d
willson733 <willsonghimire58@gmail.com>
2023-02-06 22:55:03 +0545

    commit

--- Asynchronous Js/script.js
+++ Asynchronous Js/script.js
@@ -9,5 +9,21 @@ request.open('GET', 'https://restcountries.com/v3.1/name/nepal');
 request.send();
 
 request.addEventListener('load', function () {
-  console.log(this.responseText);
+  const [data] = JSON.parse(this.responseText);
+  console.log(data);
+  console.log(data.languages);
+  const html = ` <article class="country">
+  <img class="country__img" src="${data.flags.png}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name.common}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)} people</p>
+    <p class="country__row"><span>🗣️</span>${data.languages.nep}</p>
+    <p class="country__row"><span>💰</span>${data.currencies.NPR.symbol}</p>
+  </div>
+  </article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
 });
